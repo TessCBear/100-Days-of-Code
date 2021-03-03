0520c9ec3e0e64e9bc71ec104e028422f6eb72de
Tessa Collins <tessacollins525@gmail.com>
2021-03-03 21:57:32 +0200

    Updated according to Day 29

--- Intermediate/Day 26/Day26.py
+++ Intermediate/Day 26/Day26.py
@@ -4,7 +4,14 @@ data = pandas.read_csv(".\Intermediate\Day 26\\nato_phonetic_alphabet.csv")
 
 phonetic_dictionary = {row.letter:row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-phonetic = [phonetic_dictionary[letter] for letter in word]
-print(phonetic)
+def nato_conversion():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic = [phonetic_dictionary[letter] for letter in word]
+        print(phonetic)
+    except KeyError:
+        print("Please only use letters of the alphabet.")
+        nato_conversion()
+    
+nato_conversion()
 
